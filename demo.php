d28dd6721cc8e285b0b187c9907dc3f67c43e1f8
Romil Siddhapura <romilsiddhapura@gmail.com>
2020-06-01 14:28:23 -0500

    Updated

--- demo.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-	$conn = mysqli_connect("localhost", "root", "root", "clothing"); //keep your db name
-	$sql = "SELECT * FROM products WHERE id = 1";
-	$sth = $conn->query($sql);
-	$result=mysqli_fetch_array($sth);
-	echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['product_image'] ).'"/>';
-
-?>
